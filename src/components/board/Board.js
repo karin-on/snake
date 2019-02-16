20bf4e03c3ff5a78a2aa69f949ffa048894b13df
Karina Nowicka <knowicka@gmail.com>
2019-02-16 16:56:11 +0100

    board, snake, food; snake moved

---
+++ src/components/board/Board.js
@@ -0,0 +1,38 @@
+import './Board.scss';
+
+
+class Field {
+    constructor() {
+        this.field = document.createElement('div');
+        this.fieldClass = 'board__field';
+    }
+
+    render(board) {
+        this.field.classList.add(this.fieldClass);
+        board.appendChild(this.field);
+    }
+}
+
+class Board {
+    constructor() {
+        this.board = document.createElement('div');
+        this.boardClass = 'board';
+    }
+
+    render() {
+        this.board.classList.add(this.boardClass);
+
+        const fieldsNumber = 100;
+        let start = 1;
+
+        while (start <= fieldsNumber) {
+            const field = new Field();
+            field.render(this.board);
+            start++;
+        }
+
+        document.getElementById('app').appendChild(this.board);
+    }
+}
+
+export {Board};
